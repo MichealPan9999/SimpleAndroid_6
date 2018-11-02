81f4fb3d6ab7d5ff0bbe7cf55300bb240f6fd451
MichealPan9999 <panziqiang99@gmail.com>
2018-11-02 14:23:22 +0800

    RemoteViews 控制不同进程的View的实例

---
+++ remoteviewstest/src/main/java/com/example/remoteviewstest/utils/MyConstants.java
@@ -0,0 +1,8 @@
+package com.example.remoteviewstest.utils;
+
+import android.os.Environment;
+
+public class MyConstants {
+    public static final String REMOTE_ACTION = "com.example.panzqww.action_REMOTE";
+    public static final String EXTRA_REMOTE_VIEWS = "extra_remoteViews";
+}
